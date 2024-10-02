92b3e0184e440855bc3f358b64d153d725004e9e
MMachida1461 <machida951461@gmail.com>
2024-10-03 07:41:36 +0900

    リマインド一覧のルーティング処理

--- reminder/routes/web.php
+++ reminder/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReminderController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ReminderController::class, 'index'])->name('index');
